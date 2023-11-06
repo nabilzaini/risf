fa86c847451181d5aff4752a80dc5becfd5f66c7
nabil <nabil.zaini6@gmail.com>
2023-11-06 10:39:56 +0100

    add test rounded

---
+++ src/test/java/fr/rsif/salestaxes/utils/UtilsTest.java
@@ -0,0 +1,47 @@
+package fr.rsif.salestaxes.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UtilsTest {
+
+    static Stream<Arguments> truncateDoubleToTwoDecimal() {
+        return Stream.of(
+                Arguments.of(12.49585154, 12.49),
+                Arguments.of(14.995976464, 14.99),
+                Arguments.of(10, 10),
+                Arguments.of(0.505, 0.50)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("truncateDoubleToTwoDecimal")
+    void truncateDouble(double numberLong, double numberWithTwoDecimalExpected) {
+        //GIVEN && WHEN
+        double truncatedNumberActual = Utils.truncateDouble(numberLong);
+        //THEN
+        assertEquals(numberWithTwoDecimalExpected, truncatedNumberActual);
+    }
+
+    static Stream<Arguments> rounded() {
+        return Stream.of(
+                Arguments.of(12.49585154, 12.50),
+                Arguments.of(14.995976464, 15),
+                Arguments.of(1.88, 1.9));
+    }
+
+    @ParameterizedTest
+    @MethodSource("rounded")
+    void roundOff(double number, double numberRoundedExpected) {
+        //GIVEN && WHEN
+        double numberRoundedActual = Utils.roundOff(number);
+        //THEN
+        assertEquals(numberRoundedExpected, numberRoundedActual);
+    }
+}
